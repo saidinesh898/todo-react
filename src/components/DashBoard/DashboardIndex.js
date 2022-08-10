0c235aeb3b84bcf602ecc4fc016362e1ff0d840b
Sai Dinesh <60782915+saidinesh898@users.noreply.github.com>
2022-08-10 21:49:24 +0530

    DashBoard

--- src/components/DashBoard/DashboardIndex.js
+++ src/components/DashBoard/DashboardIndex.js
@@ -1,16 +1,24 @@
 import { useDispatch, useSelector} from "react-redux";
-
 import {useEffect, useState} from 'react';
 import useHttp from '../../hooks/httpRequest'
+
+import css from './DashBoardIndex.module.css'
 
 const DashboardIndex = () => {
     const authDetails = useSelector(state=>state.auth)
     const [tasks,setTasks] = useState([])
     const loadedTask = (data, statusCode) => {
         setTasks(data)
+    
 
     }
     const {isLoading, error, sendRequest} = useHttp(loadedTask)
+    const sortOnClickHanlder = () => {
+        const helperArray = tasks.sort((a,b) => {return b.status - a.status})
+        setTasks([...helperArray])
+        console.log(tasks)
+    }
+    console.log(tasks)
     
     useEffect(() => {
         if(authDetails.token){
@@ -27,8 +35,26 @@ const DashboardIndex = () => {
     },[authDetails.token]);
 
     return (
-        <div>
-            {tasks.map((task)=> <>{`Description : ${task.description}  Status:  ${task.status}`}</>)}            
+        <div className={css.dashboardWrapper}>
+            <div className={css.sort}>
+            <button onClick={sortOnClickHanlder}>SORT</button>
+            </div>
+            <div>
+                    Your Tasks               
+            </div>
+         <div className={css.taskWrapper}>
+             {tasks && tasks.map((task)=>  
+                <div key = {task._id} className={css.tasksCard + ' ' + (task.status ? css.success : css.pending)}>
+                    <div className={css.taskDescription}>
+                        <p>Task Description</p> <p>:</p> <p><b>{task.description}</b></p>
+                    </div>
+                    <div className={css.taskStatus}>
+                    <p>Task Status &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p> <p>:</p> <p><b>{(task.status && "Completed") || (!task.status && "Pending") }</b></p>
+                    </div>
+                </div> 
+                )}
+            </div>
+        
         </div>
     );
 };
