33f37aa1377d6b62b6b95eae5c707681538a7ec5
Sai Dinesh <60782915+saidinesh898@users.noreply.github.com>
2022-08-18 19:29:22 +0530

    added Filtering and Sorting Functionalities in Dashboard

--- src/components/DashBoard/DashboardIndex.js
+++ src/components/DashBoard/DashboardIndex.js
@@ -1,25 +1,37 @@
 import { useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from 'react';
 import useHttp from '../../hooks/httpRequest'
-
+import arrow from '../../assests/img/arrow.png'
+import { useRef } from "react";
 import css from './DashBoardIndex.module.css'
 
 const DashboardIndex = () => {
+    const hideToggle = useRef()
     const authDetails = useSelector(state=>state.auth)
+    const [sortToggle, setSortToggle] = useState(true)
+    const [hideCompletedToggle, setHideCompletedToggle] = useState(false)
     const [tasks,setTasks] = useState([])
     const loadedTask = (data, statusCode) => {
         setTasks(data)
-    
-
     }
     const {isLoading, error, sendRequest} = useHttp(loadedTask)
     const sortOnClickHanlder = () => {
-        const helperArray = tasks.sort((a,b) => {return b.status - a.status})
-        setTasks([...helperArray])
-        console.log(tasks)
+        setSortToggle((prev) => !prev)
+        if(sortToggle){
+            const helperArray = tasks.sort((a,b) => {return Date.parse(b.createdAt) - Date.parse(a.createdAt)})
+            setTasks([...helperArray])
+        }
+        else{
+            const helperArray = tasks.sort((a,b) => {return Date.parse(a.createdAt) - Date.parse(b.createdAt)})
+            setTasks([...helperArray])
+        }
     }
-    console.log(tasks)
-    
+
+    const hideCompletedHandler = (e)=> {
+        hideToggle.current.checked = !hideToggle.current.checked
+        setHideCompletedToggle(hideToggle.current.checked)
+    } 
+
     useEffect(() => {
         if(authDetails.token){
             sendRequest({
@@ -34,27 +46,38 @@ const DashboardIndex = () => {
 
     },[authDetails.token]);
 
+
+    const wrapper = (task)=> {
+        return (
+        <div key = {task._id} className={css.tasksCard + ' ' + (task.status ? css.success : css.pending)}>
+          <div className={css.taskDescription}>
+              <p>Task Description</p> <p>:</p> <p><b>{task.description}</b></p>
+          </div>
+          <div className={css.taskStatus}>
+          <p>Task Status &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p> <p>:</p> <p><b>{(task.status && "Completed") || (!task.status && "Pending") }</b></p>
+          </div>
+      </div>
+        )
+    }
+    const TasksJSX = hideCompletedToggle ?
+    tasks && tasks.filter((task) => task.status!==true).map((task) => wrapper(task)) :    
+      tasks && tasks.map((task)=>  wrapper(task))
+
+    const arrowSVG = <img src={arrow} className={sortToggle ? css.arrow  : css.arrow + ' ' +  css.invertIMG}></img>
+
+    //console.log(hideToggle.current.checked)
     return (
         <div className={css.dashboardWrapper}>
             <div className={css.sort}>
-            <button onClick={sortOnClickHanlder}>SORT</button>
+            <button className={css.sortButton} onClick={sortOnClickHanlder}><p>Sort By Date </p> {arrowSVG}</button>
+            <div className={css.hideCompleted} onClick={hideCompletedHandler}> <p>Hide Completed</p><input onChange={hideCompletedHandler} type="checkbox" value="Hide Completed Task" ref={hideToggle}></input></div>
             </div>
             <div>
                     Your Tasks               
             </div>
          <div className={css.taskWrapper}>
-             {tasks && tasks.map((task)=>  
-                <div key = {task._id} className={css.tasksCard + ' ' + (task.status ? css.success : css.pending)}>
-                    <div className={css.taskDescription}>
-                        <p>Task Description</p> <p>:</p> <p><b>{task.description}</b></p>
-                    </div>
-                    <div className={css.taskStatus}>
-                    <p>Task Status &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p> <p>:</p> <p><b>{(task.status && "Completed") || (!task.status && "Pending") }</b></p>
-                    </div>
-                </div> 
-                )}
+                {TasksJSX}
             </div>
-        
         </div>
     );
 };
